021fcd20c5eeb6c1c013fc19b8bcf6cd4ff56ac2
panos <pstavrianosmail@gmail.com>
2018-06-26 23:52:45 +0300

    Initial4

--- app/src/main/java/gr/osnet/rxsocketexample/test.kt
+++ app/src/main/java/gr/osnet/rxsocketexample/test.kt
@@ -1,9 +1,12 @@
 package gr.osnet.rxsocketexample
 
+import gr.osnet.rxsocket.TestClass
+
 fun testing44(msg: String) {
     println(msg)
     println(msg)
     println(msg)
     println(msg)
-    println(msg)
+    println(TestClass().testFunc())
+
 }
