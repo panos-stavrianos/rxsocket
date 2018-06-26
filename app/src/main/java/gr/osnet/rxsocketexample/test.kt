18a0232ecedeb798d6a8fe791a2837880726a8ee
panos <pstavrianosmail@gmail.com>
2018-06-27 00:15:46 +0300

    Initial5

--- app/src/main/java/gr/osnet/rxsocketexample/test.kt
+++ app/src/main/java/gr/osnet/rxsocketexample/test.kt
@@ -1,12 +1,13 @@
 package gr.osnet.rxsocketexample
 
 import gr.osnet.rxsocket.TestClass
+import gr.osnet.rxsocket.meta.CRC16Hex
 
 fun testing44(msg: String) {
     println(msg)
     println(msg)
     println(msg)
-    println(msg)
+    println(CRC16Hex.addCheck(msg))
     println(TestClass().testFunc())
-
+    CRC16Hex.addCheck(msg)
 }
